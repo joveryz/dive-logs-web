5e891a3f9fc40510c118fd9ef00dab970f89be10
Jovery Zhang <jovery@zmailing.com>
2025-12-30 10:43:18 +0800

    Fix build error

--- src/components/DiveDetail/DiveDetail.tsx
+++ src/components/DiveDetail/DiveDetail.tsx
@@ -668,12 +668,3 @@ function InputField({ value }: { value: string }) {
     </div>
   );
 }
-
-function EmptyState({ message, icon = '📋' }: { message: string; icon?: string }) {
-  return (
-    <div className="flex flex-col items-center justify-center h-40 text-gray-500">
-      <span className="text-4xl mb-3">{icon}</span>
-      <span>{message}</span>
-    </div>
-  );
-}
